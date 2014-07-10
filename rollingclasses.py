e4a99234c396805769a281e84a97d6f319761194
loneshaman <spinnipati2@gmail.com>
2014-07-10 16:29:33 -0700

    Buttons work! rollingclasses.py now deprecated, do not use.

--- rollingclasses.py
+++ rollingclasses.py
@@ -1,6 +1,8 @@
 from Tkinter import *
 
 class ChoiceWindow(Frame):
+	def get_choice(self):
+		return self.button_choice
 	def __init__(self, master):
 		Frame.__init__(self, master)
 		self.grid()
@@ -11,9 +13,9 @@ class ChoiceWindow(Frame):
 	def create_widgets(self):
 		self.type_label = Label(self, text = "What type of roll would you like to make?")
 		self.type_label.grid(row = 1, sticky = W)
-		self.atk_button = Radiobutton(self, text="Attack", variable=self.button_choice, value="atk")
+		self.atk_button = Radiobutton(self, text="Attack", variable=self.button_choice, value="atk", command = get_choice)
 		self.atk_button.grid(row = 2, sticky = W)
-		self.init_button= Radiobutton(self, text="Initiative", variable=self.button_choice, value="init")
+		self.init_button= Radiobutton(self, text="Initiative", variable=self.button_choice, value="init", command = get_choice)
 		self.init_button.grid(row = 3, sticky = W)
-		self.skill_button = Radiobutton(self, text="Skill", variable=self.button_choice, value="skil")
+		self.skill_button = Radiobutton(self, text="Skill", variable=self.button_choice, value="skil", command = get_choice)
 		self.skill_button.grid(row = 4, sticky = W)
--- rollingguipy.py
+++ rollingguipy.py
@@ -1,8 +1,26 @@
 from Tkinter import *
-from rollingclasses import *
+
+
+def get_choice():
+	root.destroy()
 
 root = Tk()
 root.title("Anima Roll Formatter")
-root.geometry("300x120")
-app = ChoiceWindow(root)
+root.geometry("120x120")
+button_choice = StringVar()
+app = Frame(root)
+app.pack_propagate(0) # don't shrink
+app.grid()
+atk_button = Radiobutton(app, indicatoron = 0, text="Attack", variable=button_choice, value="atk", command = get_choice)
+atk_button.grid(row = 2, sticky = W)
+init_button= Radiobutton(app, indicatoron = 0, text="Initiative", variable=button_choice, value="init", command = get_choice)
+init_button.grid(row = 3, sticky = W)
+skill_button = Radiobutton(app, indicatoron = 0, text="Skill", variable=button_choice, value="skil", command = get_choice)
+skill_button.grid(row = 4, sticky = W)
 root.mainloop()
+if button_choice.get() == 'atk':
+	print("Attack chosen")
+if button_choice.get() == 'init':
+	print("Initiative chosen")
+if button_choice.get() == 'skil':
+	print("Skill chosen")
